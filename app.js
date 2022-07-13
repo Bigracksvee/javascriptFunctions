7fe23f4adea1f9c881f8698d653d4c4c4c8ebbe2
Vera <veraopoku100@gmail.com>
2022-07-13 06:43:58 -0400

    pushing with javascriptFunctions

---
+++ app.js
@@ -0,0 +1,37 @@
+var x = 5;
+var y = 4;
+
+function sum(param1, param2){
+    console.log(param1 + param2);
+}
+sum(x, y);
+
+
+
+var x = 5;
+var y = 4;
+
+function difference(param1, param2){
+    console.log(param1 - param2);
+}
+difference(x, y);
+
+
+
+var x = 5;
+var y = 4;
+
+function multiplication(param1, param2){
+    console.log(param1 * param2);
+}
+multiplication(x, y);
+
+
+
+var x = 5;
+var y = 4;
+
+function division(param1, param2){
+    console.log(param1 / param2);
+}
+division(x, y);
